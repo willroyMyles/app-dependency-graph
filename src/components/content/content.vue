4aa400f581652602e40ab5c412ee647df01c33f5
Willroy Myles <willroymyles@yahoo.com>
2021-10-29 13:08:05 -0500

    Updated content file

--- src/components/content/content.vue
+++ src/components/content/content.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <v-container> -->
-      <v-col id="nodes" />
+  <v-col id="nodes" />
   <!-- </v-container> -->
 </template>
 
@@ -18,13 +18,17 @@ export default defineComponent({
   name: "Content",
   setup() {
     let state = reactive({
-      currentNode : null as d3.Selection<any, unknown, null, undefined> | null,
-      nodes: [new NodeModel("esif"), new NodeModel("web admin"), new NodeModel("Reward service")],
+      currentNode: null as d3.Selection<any, unknown, null, undefined> | null,
+      nodes: [
+        new NodeModel("esif"),
+        new NodeModel("web admin"),
+        new NodeModel("Reward service"),
+      ],
     });
 
     const generateNodes = () => {
       let radius = 50;
-      let width = window.outerWidth ;
+      let width = window.outerWidth;
       let height = window.innerHeight - 70;
 
       const svg = d3
@@ -32,7 +36,6 @@ export default defineComponent({
         .attr("viewBox", ["0", "0", width.toString(), height.toString()])
         .style("background-color", "lightgrey");
       d3.select("#nodes").append(() => svg.node());
-
 
       d3.range(state.nodes.length).map((i) => {
         state.nodes[i].x = Math.random() * (width - radius * 2) + radius;
@@ -49,93 +52,78 @@ export default defineComponent({
           return d.x;
         })
         .attr("y", (d) => d.y)
-        .attr("z-index", 0)
+        .attr("z-index", 0);
 
-
-        const circles = node
+      const circles = node
         .append("circle")
         .attr("fill", (d, i) => color(i.toString()))
         .attr("stroke", 2)
         .attr("r", radius)
-        .attr("cx", (v,i,e) => v.x)
-        .attr("cy", d => d.y)
+        .attr("cx", (v, i, e) => v.x)
+        .attr("cy", (d) => d.y)
         .attr("z-index", 0)
-        .call( d3
-        .drag()
-        .on("start", dragstarted)
-        .on("drag", dragged).on("end", dragended)
-       )
+        .call(
+          d3
+            .drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+        );
 
-        const texts = node
+      const texts = node
         .append("text")
-        .text(d => d.name)
-        .attr("x", d => d.x )
-        .attr("y", d => d.y + radius * 1.5)
-        .attr("text-anchor", "middle")
+        .text((d) => d.name)
+        .attr("x", (d) => d.x)
+        .attr("y", (d) => d.y + radius * 1.5)
+        .attr("text-anchor", "middle");
 
-
-    
       function dragstarted(e: any, d: any) {
-        var el = d3.select(e.sourceEvent.srcElement)
+        var el = d3.select(e.sourceEvent.srcElement);
         state.currentNode = el;
-        
       }
 
       function dragged(e: any, d: any) {
-        var el = e.sourceEvent.srcElement;    
+        var el = e.sourceEvent.srcElement;
 
-        state.currentNode!
-          .attr("cx",  d.x = e.x)
-          .attr("cy", d.y =  e.y)
+        state.currentNode!.attr("cx", (d.x = e.x)).attr("cy", (d.y = e.y));
 
-let parent = d3.select(state.currentNode!.node().parentNode);
+        let parent = d3.select(state.currentNode!.node().parentNode);
 
-        parent.select("text")
+        parent
+          .select("text")
           .attr("x", (d.x = e.x))
           .attr("y", (d.y = e.y + 75));
-
       }
 
       function dragended(e: any, d: any) {
-        d3.select(e.sourceEvent.srcElement).classed("active", false).attr("z-index", 0)
-        state.currentNode = null
+        d3.select(e.sourceEvent.srcElement)
+          .classed("active", false)
+          .attr("z-index", 0);
+        state.currentNode = null;
       }
 
-      const zoom = d3.zoom()
-      .scaleExtent([0.5, 62])
-      .on("zoom", zoomed);
+      const zoom = d3.zoom().scaleExtent([0.5, 62]).on("zoom", zoomed);
 
-      let k = height / width
+      let k = height / width;
 
-     var x = d3.scaleLinear()
-    .domain([-4.5, 4.5])
-    .range([0, width])
+      var x = d3.scaleLinear().domain([-4.5, 4.5]).range([0, width]);
 
-    let y = d3.scaleLinear()
-    .domain([-4.5 * k, 4.5 * k])
-    .range([height, 0])
+      let y = d3
+        .scaleLinear()
+        .domain([-4.5 * k, 4.5 * k])
+        .range([height, 0]);
 
+      function zoomed({ transform }: { transform: d3.ZoomTransform }) {
+        const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
+        const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
+        node.attr("transform", transform);
+      }
 
-      function zoomed({transform } : {transform : d3.ZoomTransform}) {
-        console.log("called", transform);
-        
-    const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
-    const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
-    node.attr("transform", transform)
-  }
-
-
-
-  // apply zoom
-  svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
-
-
+      // apply zoom
+      svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
       console.log("generated nodes");
     };
 
-  
-
-   
     onMounted(() => {
       generateNodes();
     });
--- src/models/node.ts
+++ src/models/node.ts
@@ -1,14 +1,18 @@
 import {uuid} from "vue-uuid";
+import DatabaseModel from "./databaseModel";
 
 export default class NodeModel {
     id = uuid.v4()
     name : string
     x : number
     y : number
+    hasDatabase = false
+    databaseModel : DatabaseModel | null
     
     constructor(name = "random"){
         this.x = 0.0;
         this.y = 0.0;
         this.name = name;
+        this.databaseModel = null
     }
 }
