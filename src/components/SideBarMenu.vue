021adc689aca3367f1026e32cdcf54c627151856
Willroy Myles <willroymyles@yahoo.com>
2021-11-06 23:11:38 -0500

    Added debounce when searching

--- src/components/SideBarMenu.vue
+++ src/components/SideBarMenu.vue
@@ -4,11 +4,11 @@
       <!-- <a-layout-content style="height: 90vh; display : flex"> -->
         <div class="mounted content">
           <a-row justify="space-between">
-            <a-col span="5">fliter by tag : </a-col>
-            <a-col >
-              <a-select style="width: 100px" mode="multiple" placeholder="tags" :value="currentTags" @change="handleTagChange">
+            <a-col span="9">fliter by tag : </a-col>
+            <a-col span="" >
+              <a-select style="width: 100px" mode="multiple" placeholder="tags" :value="currentTags" @change="handleTagChange" >
                 <a-select-option
-                  v-for="tag in store.getTags()"
+                  v-for="tag in tagoptions"
                   :key="tag"
                   :value="tag"
                   >{{ tag }}</a-select-option
@@ -17,14 +17,14 @@
             </a-col>
           </a-row>
           <a-row justify="space-between">
-            <a-col span = 5> search </a-col>
+            <a-col span ="9"> search </a-col>
             <a-col >
-              <a-select style="width: 100px" mode="default" placeholder="search" :value="pattern" @change="handleTagChange">
+              <a-select ref="searchRef" style="width: 100px" mode="multiple" placeholder="search" :value="pattern" @search="searchByName" @change="handleSearchChange" >
                 <a-select-option
-                  v-for="tag in store.getTags()"
-                  :key="tag"
-                  :value="tag"
-                  >{{ tag }}</a-select-option
+                  v-for="node in searchResults"
+                  :key="node.id"
+                  :value="node.name"
+                  >{{ node.name }}</a-select-option
                 >
               </a-select>
             </a-col>
@@ -54,31 +54,56 @@ import {
 } from "vue";
 import { store } from "@/store/DataStoreage";
 import NodeModel from "@/models/node";
+import debounce from 'debounce-fn'
+import { Timer } from "d3";
+import debounceFn from "debounce-fn";
+
 
 export default defineComponent({
   name: "SideBarMenu",
 
   setup() {
     const state = reactive({
-      currentTags: Array.from(store.state.filter.tags) as string[],
-      pattern : "" as string | null,
+      currentTags: [] as string[],
+      tagoptions : [] as string[],
+      pattern : [] as string[],
       searchResults : [] as NodeModel[]
     });
 
+    const debounceFunction = debounce((arg : string) => {
+        state.searchResults = store.searchByName(arg)
+        if(arg == "") state.searchResults = []
+      }, {wait : 2000})
+
+    const searchRef = ref();
+
+    onMounted(()=>{
+      state.currentTags = Array.from(store.state.filter.tags)
+      state.tagoptions = store.getTags()
+    })
+
     function filterByTag() {
+      console.log(state.currentTags);
+      
       store.filterByTag(state.currentTags)
     }
 
     function handleTagChange(tag : string[]){
+      console.log(tag);
+      
       state.currentTags = tag
     }
 
-    function handleSearchChange(patter : string){
-
+    function handleSearchChange(pattern : string[]){
+      searchRef.value.blur()
+      console.log(pattern);
+      // should highlight node
+      
     }
 
-    function searchByName(){
-
+    function searchByName(pattern : string){
+      //should debounce and then search
+      debounceFunction(pattern as any)
     }
 
     return {
@@ -87,11 +112,16 @@ export default defineComponent({
       filterByTag,
       searchByName,
       handleTagChange,
-      handleSearchChange
+      handleSearchChange,
+      searchRef
       
     };
   },
 });
+
+function timer(arg0: (elasped: any) => void): Timer | null {
+throw new Error("Function not implemented.");
+}
 </script>
 
 <style scoped>
